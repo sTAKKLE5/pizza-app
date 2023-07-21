e14639deeb1fd11d591f642ebc9965294301742b
Thomas Rommel <thomasrommel89@gmail.com>
2023-07-21 12:35:50 +0200

    frontend for direct dough added

--- pizza_app/api/routes/api.go
+++ pizza_app/api/routes/api.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	bigaDoughHandler "pizza-app/pizza_app/api/handlers/dough/biga"
 	directDoughHandler "pizza-app/pizza_app/api/handlers/dough/direct"
 	poolishDoughHandler "pizza-app/pizza_app/api/handlers/dough/poolish"
@@ -9,6 +10,13 @@ import (
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/dough/direct", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "direct.html", gin.H{
+			"url": "http://localhost:8080/dough/direct", // FIXME: This is hardcoded
+		})
+	})
 
 	router.POST("/dough/direct", directDoughHandler.HandleDirectDough)
 	router.POST("/dough/biga", bigaDoughHandler.HandleBigaDough)
