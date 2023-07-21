e0bbb757e05eb2e032d90924e0febf0742cdff46
Thomas Rommel <thomasrommel89@gmail.com>
2023-07-21 12:18:12 +0200

    direct dough added

--- pizza_app/models/request/request.go
+++ pizza_app/models/request/request.go
@@ -1,7 +1,7 @@
 package models
 
 type DoughRequest struct {
-	DoughBallAmount int     `json:"doughBallAmount"`
-	Hydration       float64 `json:"hydration"`
-	DoughBallWeight float64 `json:"doughBallWeight"`
+	DoughBallAmount int     `json:"doughBallAmount"` // In number of dough balls
+	Hydration       float64 `json:"hydration"`       // In percentage from 0 to 100
+	DoughBallWeight float64 `json:"doughBallWeight"` // In grams
 }
