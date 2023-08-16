8ba8b45f41014ab960a485d4e2fd0c78d6bf8606
Thomas Rommel <thomasrommel89@gmail.com>
2023-08-16 10:07:22 +0200

    refactoring

--- pizza_app/api/routes/api.go
+++ pizza_app/api/routes/api.go
@@ -12,7 +12,7 @@ func SetupRouter() *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 
-	router.LoadHTMLGlob("templates/*")
+	router.LoadHTMLGlob("templates/**/*")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
