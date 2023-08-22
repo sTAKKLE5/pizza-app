80bf0ba01d60adb5c87830095823a9681c41a669
Thomas Rommel <thomasrommel89@gmail.com>
2023-08-22 12:38:25 +0200

    bootstrap5 added

--- pizza_app/api/routes/api.go
+++ pizza_app/api/routes/api.go
@@ -15,25 +15,31 @@ func SetupRouter() *gin.Engine {
 	router.LoadHTMLGlob("templates/**/*")
 
 	// add favicon
-	router.StaticFile("/favicon.ico", "./static/favicon.ico")
+	router.StaticFile("/favicon.ico", "./static/img/favicon.ico")
+	router.Static("/css", "./static/css")
+	router.Static("/img", "./static/img")
+	router.Static("/js", "./static/js")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
+		c.HTML(http.StatusOK, "index.html", gin.H{"currentPath": c.Request.URL.Path})
 	})
 	router.GET("dough/biga", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "biga.html", gin.H{
-			"url": "http://localhost:8080/dough/biga",
+			"url":         "http://localhost:8080/dough/biga",
+			"currentPath": c.Request.URL.Path,
 		})
 	})
 	router.GET("dough/poolish", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "poolish.html", gin.H{
-			"url": "http://localhost:8080/dough/poolish",
+			"url":         "http://localhost:8080/dough/poolish",
+			"currentPath": c.Request.URL.Path,
 		})
 	})
 
 	router.GET("/dough/direct", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "direct.html", gin.H{
-			"url": "http://localhost:8080/dough/direct",
+			"url":         "http://localhost:8080/dough/direct",
+			"currentPath": c.Request.URL.Path,
 		})
 	})
 
