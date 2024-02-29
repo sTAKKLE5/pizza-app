ab6ac55ef2aec13975225817270a0b613925bd28
Thomas Rommel <thomasrommel89@gmail.com>
2024-02-29 15:38:47 +0100

    port added

--- pizza_app/main.go
+++ pizza_app/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"os"
 	"pizza-app/pizza_app/api/middleware"
 	"pizza-app/pizza_app/api/routes"
 )
 
 func main() {
 	r := routes.SetupRouter()
-	err := r.Run()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	err := r.Run(":" + port)
 	middleware.HandleError(err)
 }
